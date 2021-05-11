59ca5cd46d0457cc48ddf6816d2890cb154348ff
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-05-11 14:10:30 -0400

    localStorage - useEffect

--- src/App.js
+++ src/App.js
@@ -1,57 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import Formulario from './components/Formulario';
-import Cita from './components/Cita';
-
+import React, { useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
+  // Citas en local storage
+  let citasIniciales = localStorage.getItem("citas")
+    ? JSON.parse(localStorage.getItem("citas"))
+    : [];
 
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
 
   useEffect(() => {
-    console.log('Documento listo o algo paso con las citas');
+
+    if(citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas));
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
   }, [citas]);
 
   const crearCita = cita => {
-    guardarCitas([
-      ...citas,
-      cita
-    ]);
-  }
-  
+    guardarCitas([...citas, cita]);
+  };
+
   // Eliminar cita por su id
   const eliminarCita = id => {
     const nuevasCitas = citas.filter(cita => cita.id !== id);
     guardarCitas(nuevasCitas);
-  }
+  };
 
   // Mensaje condicional
-  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas";
 
   return (
     <>
-    <h1>Administador de Pacientes</h1>
+      <h1>Administador de Pacientes</h1>
 
-    <div className="container">
-      <div className="row">
-        <div className="one-half column">
-          <Formulario 
-            crearCita={crearCita}
-          />
-        </div>
-        <div className="one-half column">
-          <h2>{titulo}</h2>
-          {
-            citas.map(cita => (
-              <Cita 
-                key={cita.id}
-                cita={cita}
-                eliminarCita={eliminarCita}
-              />
-            ))
-          }
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita} />
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map(cita => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
