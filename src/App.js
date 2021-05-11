e249b505f950681ad08098d466fe44f0a727d01b
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-05-11 10:29:37 -0400

    Administrador de citas - formulario

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Formulario from './components/Formulario';
+
+function App() {
+  return (
+    <>
+    <h1>Administador de Pacientes</h1>
+
+    <div className="container">
+      <div className="row">
+        <div className="one-half column">
+          <Formulario />
+        </div>
+        <div className="one-half column">
+          2
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
